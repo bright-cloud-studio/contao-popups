1932af3f8f8362cc7deb89050eb29b9fc477041d
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-19 13:14:50 -0400

    Create config.php

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,19 @@
+<?php
+ 
+/**
+* @copyright  Bright Cloud Studio
+* @author     Bright Cloud Studio
+* @package    Contao CE Recaptcha
+* @license    LGPL-3.0+
+* @see	       https://github.com/bright-cloud-studio/contao-popups
+*/
+
+//use Bcs\PopupBundle\FormRecaptcha;
+
+// Setup our Form field
+//array_insert($GLOBALS['TL_FFL']['recaptcha'], -1, FormRecaptcha::class);
+
+// Setup our background content element
+//array_insert($GLOBALS['TL_CTE']['miscellaneous'], 0, [
+//    'backgroundrecaptcha' => Bcs\ElementRecaptcha::class,
+//]);
