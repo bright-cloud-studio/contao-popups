5f8989546e9290dfb00c0e88f4cd5af63751c3e1
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-23 15:14:11 -0400

    Update tl_article.php

--- src/Resources/contao/dca/tl_article.php
+++ src/Resources/contao/dca/tl_article.php
@@ -1,18 +1,2 @@
 <?php
 
-
-/* Add Custom Fields */
-$GLOBALS['TL_DCA'['tl_article'['fields']['popup'] = array(
-  'label' => &$GLOBALS['TL_LANG']['tl_article']['popup'],
-  'inputType' => 'checkbox',
-  'eval' => array('submitOnChange'=>true),
-  'sql' => "char(1) NOT NULL default ''"
-);
-
-$GLOBALS['TL_DCA']['tl_article']['fields']['popup_class'] = array
-(
-	'label'                   => &$GLOBALS['TL_LANG']['tl_article']['popup_class'],
-	'inputType'               => 'text',
-	'eval'                    => array('tl_class'=>'w50'),
-	'sql'                     => "varchar(255) NOT NULL default ''"
-);
