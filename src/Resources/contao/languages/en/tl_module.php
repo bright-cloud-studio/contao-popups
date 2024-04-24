0302ed9059baa751424c3c0a75cf3b1006e65e18
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-24 15:10:01 -0400

    Create tl_module.php

---
+++ src/Resources/contao/languages/en/tl_module.php
@@ -0,0 +1,34 @@
+<?php
+ 
+/**
+ * Zyppy Popup
+ *
+ * Copyright (C) 2018-2022 Andrew Stevens Consulting
+ *
+ * @package    asconsulting/zyppy_popup
+ * @link       https://andrewstevens.consulting
+ */
+
+ 
+ 
+/**
+ * Fields
+ */
+
+$GLOBALS['TL_LANG']['tl_module']['popup'] 					= array("Popup", "Is this module a popup");
+$GLOBALS['TL_LANG']['tl_module']['popupClass'] 				= array("CSS Class", "CSS class to add to popup.");
+$GLOBALS['TL_LANG']['tl_module']['popupDelay'] 				= array("Delay", "Delay in milliseconds before popup is shown.");
+$GLOBALS['TL_LANG']['tl_module']['popupReshowDelay'] 		= array("Reshow Delay", "Delay in minutes before reshowing a popup.");
+$GLOBALS['TL_LANG']['tl_module']['popupScrollTrigger'] 		= array("Scroll Trigger", "Trigger popup after scrolling x pixels.");
+$GLOBALS['TL_LANG']['tl_module']['popupFadeDuration'] 		= array("Fade Duration", "Duration of fadeout effect in milliseconds.");
+$GLOBALS['TL_LANG']['tl_module']['popupTrigger'] 			= array("Trigger", "CSS selector that will trigger the popup.");
+$GLOBALS['TL_LANG']['tl_module']['popupUuid'] 				= array("Unique ID", "Auto generated unique ID for popup.");
+$GLOBALS['TL_LANG']['tl_module']['popupAddClose'] 			= array("Add Close Button", "Add a close button to the popup.");
+$GLOBALS['TL_LANG']['tl_module']['popupClear'] 				= array("Clear on Popup", "Clear results when popup opened.");
+$GLOBALS['TL_LANG']['tl_module']['popupAccept'] 			= array("Accept Popup", "User must accept popup.");
+$GLOBALS['TL_LANG']['tl_module']['popupRejectUrl'] 			= array("Popup Reject", "Where to redirect user if popup is rejected.");
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_module']['popup_legend'] 			= 'Popup Settings';
