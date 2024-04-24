9160e136b7e54039febb3452e32f99434359dead
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-04-24 15:08:50 -0400

    Create default.php

---
+++ src/Resources/contao/languages/en/default.php
@@ -0,0 +1,17 @@
+<?php
+ 
+/**
+ * Zyppy Popup
+ *
+ * Copyright (C) 2018-2022 Andrew Stevens Consulting
+ *
+ * @package    asconsulting/zyppy_popup
+ * @link       https://andrewstevens.consulting
+ */
+
+ 
+ 
+/**
+ * Layout Columns
+ */
+$GLOBALS['TL_LANG']['COLS']['popup'] = "Pop-up";
