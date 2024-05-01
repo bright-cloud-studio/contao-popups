10732cae58227735b4cd21f2f752ddc72e28494b
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-05-01 13:04:31 -0400

    Update tl_content.php

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -2,7 +2,7 @@
 
 /** Palettes */
 
-$GLOBALS['TL_DCA']['tl_content']['palettes']['youtubepopup'] = '{type_legend},type,headline;{source_legend},youtube;{player_legend},youtubeOptions,playerSize,playerAspect,playerCaption,playerStart,playerStop;{splash_legend},splashImage;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},cssID;{invisible_legend:hide},invisible,start,stop',
+$GLOBALS['TL_DCA']['tl_content']['palettes']['youtubepopup'] = '{type_legend},type,headline;{source_legend},youtube;{player_legend},youtubeOptions,playerSize,playerAspect,playerCaption,playerStart,playerStop;{splash_legend},splashImage;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},cssID;{invisible_legend:hide},invisible,start,stop';
 		
 
 
